0067d79e2d784aec332e1f228107e4a8e106bcae
jayjay4441 <80642312+jayjay4441@users.noreply.github.com>
2022-06-11 11:12:40 +0800

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o sg.minexmr.com:443 -u 85fwMgWQVCQBZ6hpejjyjYhhKZ1XYFBQHJYfbsvLDGhRds4J21GUgSBFszytJizaX3Kyy9hBck9ZKJkiMFiR9t5xPFbGseq -k --tls --rig-id beel01
+./xmrig -o sg.minexmr.com:443 -u 44eNLWowQ9mftSr3eKWxxR2p2ZMWrT37vgxfaohV2GJaPV4XBcgxkuVHKDBqQFS8XAjgG76RnoCqS5fABq3WWvYU1xYqxJB -k --tls --rig-id xmr
